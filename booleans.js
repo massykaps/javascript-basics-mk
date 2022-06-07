f61dc9982342fdc7d14f4a049cc31e90a926d1ed
massykaps <massykaps@users.noreply.github.com>
2022-06-07 21:07:56 +0100

    objects tests completed

---
+++ booleans.js
@@ -0,0 +1,77 @@
+function negate(a) {
+  // your code here
+};
+
+function both(a, b) {
+  // your code here
+};
+
+function either(a, b) {
+  // your code here
+};
+
+function none(a, b) {
+  // your code here
+};
+
+function one(a, b) {
+  // your code here
+};
+
+function truthiness(a) {
+  // your code here
+};
+
+function isEqual(a, b) {
+  // your code here
+};
+
+function isGreaterThan(a, b) {
+  // your code here
+};
+
+function isLessThanOrEqualTo(a, b) {
+  // your code here
+};
+
+function isOdd(a) {
+  // your code here
+};
+
+function isEven(a) {
+  // your code here
+};
+
+function isSquare(a) {
+  // your code here
+};
+
+function startsWith(char, string) {
+  // your code here
+};
+
+function containsVowels(string) {
+  // your code here
+};
+
+function isLowerCase(string) {
+  // your code here
+};
+
+module.exports = {
+  negate,
+  both,
+  either,
+  none,
+  one,
+  truthiness,
+  isEqual,
+  isGreaterThan,
+  isLessThanOrEqualTo,
+  isOdd,
+  isEven,
+  isSquare,
+  startsWith,
+  containsVowels,
+  isLowerCase
+};
